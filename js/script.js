adea6b0251a27bcb804ebf1ebb0f472329497eaa
kcccendana <kc_telyn@yahoo.com>
2023-06-19 23:12:09 +0900

    add jvascript for menu bar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-// var nav_button = document.getElementById("nav-button");
+var nav_button = document.getElementById("nav-button");
+var nav_list = document.querySelector(".desktop-view-links");
+
+nav_button.addEventListener('click', () => {
+    nav_list.classList.toggle('show');
+
+});
+
