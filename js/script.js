35a1cfc08b2e155b300215f630dd8fe592e2794a
kcccendana <kc_telyn@yahoo.com>
2023-06-20 08:49:25 +0900

    add on scroll program to highligh the nav menu

--- js/script.js
+++ js/script.js
@@ -15,4 +15,27 @@ navMenu.forEach((links) => links.addEventListener('click', () => {
   logo.classList.remove('hide');
 }));
 
+window.addEventListener('scroll', () => {
+  const sections = document.querySelectorAll('section');
+  const menuItems = document.querySelectorAll('.desktop-view-links li');
+
+  // Get the current scroll position
+  const currentPosition = window.scrollY || window.scrollY;
+
+  // Loop through each section and check if it's in the viewport
+  sections.forEach((section, index) => {
+    const sectionTop = section.offsetTop - 20; // Adjust 50 as needed
+
+    if (currentPosition >= sectionTop) {
+      // Remove highlight from all menu items
+      menuItems.forEach((item) => {
+        item.classList.remove('active');
+      });
+
+      // Add highlight to the corresponding menu item
+      menuItems[index].classList.add('active');
+    }
+  });
+});
+
 window.onresize = () => { if (window.innerWidth > 760) navList.classList.remove('show'); };
