f1851b07695c04ac02cb609a9a2552b09790fe6c
kcccendana <kc_telyn@yahoo.com>
2023-06-20 03:12:04 +0900

    modify javascript for nav menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-var nav_button = document.getElementById("nav-button");
-var nav_list = document.querySelector(".desktop-view-links");
+/*  eslint linebreak-style: ["error", "windows"]    */
+const navButton = document.querySelector('.nav-button');
+const navList = document.querySelector('.desktop-view-links');
+const navMenu = document.querySelectorAll('.navMenu');
+const logo = document.querySelector('.logo');
 
-nav_button.addEventListener('click', () => {
-    nav_list.classList.toggle('show');
+navButton.addEventListener('click', () => {
+  navList.classList.toggle('show');
+  navButton.classList.toggle('close');
+  logo.classList.toggle('hide');
+}, false);
 
-});
+navMenu.forEach((links) => links.addEventListener('click', () => {
+  navList.classList.remove('show');
+  navButton.classList.remove('close');
+  logo.classList.remove('hide');
+}));
 
+window.onresize = () => { if (window.innerWidth > 760) navList.classList.remove('show'); };
