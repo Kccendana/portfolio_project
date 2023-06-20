e9bc356170e50d3f6521047d703a78f4d86bf51e
Christopher Uduekwe <christopheruduekwe@gmail.com>
2023-06-20 19:09:59 +0100

    Update script.js
    Create javascript to handle popup modal.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,81 @@
-const navButton = document.querySelector('.nav-button');
-const navList = document.querySelector('.desktop-view-links');
-const navMenu = document.querySelectorAll('.navMenu');
-const logo = document.querySelector('.logo');
-
-navButton.addEventListener('click', () => {
-  navList.classList.toggle('show');
-  navButton.classList.toggle('close');
-  logo.classList.toggle('hide');
-}, false);
-
-navMenu.forEach((links) => links.addEventListener('click', () => {
-  navList.classList.remove('show');
-  navButton.classList.remove('close');
-  logo.classList.remove('hide');
-}));
-
-window.onresize = () => { if (window.innerWidth > 760) navList.classList.remove('show'); };
+const projectsLists = [
+  {
+    name: 'Keeping track of hundreds  of components website',
+    image: 'SnapshootPortfolio.svg',
+    imageDescription: 'Snapshoot portfolio',
+    description:
+    ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s."],
+    tags: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: '#',
+    sourceLink: '#',
+  },
+  {
+    name: 'Multi-Post Stories',
+    image: 'ImgPlaceholder-desk.svg',
+    imageDescription: 'Img Placeholder',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required. This has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    techList: ['HTML', 'CSS', 'Bootstrap', 'Ruby'],
+    liveLink: '#',
+    sourceLink: '#',
+  },  {
+    name: 'Profesional Art Printing Data More',
+    image: 'ImgPlaceholder.svg',
+    imageDescription: 'Image Design',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industrys standard.',
+    tags: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: '#',
+    sourceLink: '#',
+  },
+  {
+    name: 'Data Dashboard Healthcare',
+    image: 'Portfolio3.svg',
+    imageDescription: 'Portfolio image',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industrys standard.',
+    tags: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: '#',
+    sourceLink: '#',
+  },
+  {
+    name: 'Website Portfolio',
+    image: 'Portfolio.svg',
+    imageDescription: 'Portfolio image placeholder',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industrys standard.',
+    tags: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: '#',
+    sourceLink: '#',
+  },
+  {
+    name: 'Profesional Art Printing Data More',
+    image: 'ImgPlaceholder.svg',
+    imageDescription: 'Image Design',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industrys standard.',
+    tags: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: '#',
+    sourceLink: '#',
+  },
+  {
+    name: 'Data Dashboard Healthcare',
+    image: 'Portfolio3.svg',
+    imageDescription: 'Portfolio image',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industrys standard.',
+    tags: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: '#',
+    sourceLink: '#',
+  },
+  {
+    name: 'Website Portfolio',
+    image: 'Portfolio3.svg',
+    imageDescription: 'Portfolio image placeholder',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industrys standard.',
+    tags: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: '#',
+    sourceLink: '#',
+  },
+];
